b22dccc3b0c13afb0372d685684abf53b14337ff
Jean Pierre <jeanpierre@gitpod.io>
2023-09-21 20:41:19 +0000

    Fix telemetry event

--- src/vs/gitpod/common/insightsHelper.ts
+++ src/vs/gitpod/common/insightsHelper.ts
@@ -84,185 +84,180 @@ function doMapMetrics(source: 'window' | 'remote-server', eventName: string, dat
 
 // please don't send same metrics from browser window and remote server
 export function mapTelemetryData(source: 'window' | 'remote-server', eventName: string, data: any): { event: string; properties: { [key: string]: any } } | undefined {
-	if (source === 'remote-server') {
-		if (eventName.startsWith('extensionGallery:')) {
-			const operation = eventName.split(':')[1];
-			if (operation === 'install' || operation === 'update' || operation === 'uninstall') {
-				return {
-					event: 'vscode_extension_gallery',
-					properties: {
-						kind: operation,
-						extensionId: data.id,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp,
-						success: data.success,
-						errorCode: data.errorcode,
-					},
-				};
+	if (eventName.startsWith('extensionGallery:')) {
+		const operation = eventName.split(':')[1];
+		if (operation === 'install' || operation === 'update' || operation === 'uninstall') {
+			return {
+				event: 'vscode_extension_gallery',
+				properties: {
+					kind: operation,
+					extensionId: data.id,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp,
+					success: data.success,
+					errorCode: data.errorcode,
+				},
+			};
+		}
+	}
+	switch (eventName) {
+		case 'editorOpened':
+			if (readAccessTracked || (<string>data.typeId) !== 'workbench.editors.files.fileEditorInput') {
+				return undefined;
 			}
-		}
-		switch (eventName) {
-			case 'editorOpened':
-				if (readAccessTracked || (<string>data.typeId) !== 'workbench.editors.files.fileEditorInput') {
-					return undefined;
-				}
-				readAccessTracked = true;
-				return {
-					event: 'vscode_file_access',
-					properties: {
-						kind: 'read',
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'filePUT':
-				if (writeAccessTracked) {
-					return undefined;
-				}
-				writeAccessTracked = true;
-				return {
-					event: 'vscode_file_access',
-					properties: {
-						kind: 'write',
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'notification:show':
-				return {
-					event: 'vscode_notification',
-					properties: {
-						action: 'show',
-						notificationId: data.id,
-						source: data.source,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'notification:close':
-				return {
-					event: 'vscode_notification',
-					properties: {
-						action: 'close',
-						notificationId: data.id,
-						source: data.source,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'notification:hide':
-				return {
-					event: 'vscode_notification',
-					properties: {
-						action: 'hide',
-						notificationId: data.id,
-						source: data.source,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'notification:actionExecuted':
-				return {
-					event: 'vscode_notification',
-					properties: {
-						action: 'actionExecuted',
-						notificationId: data.id,
-						source: data.source,
-						actionLabel: data.actionLabel,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'settingsEditor.settingModified':
-				return {
-					event: 'vscode_update_configuration',
-					properties: {
-						key: data.key,
-						target: data.target,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'gettingStarted.ActionExecuted':
-				return {
-					event: 'vscode_getting_started',
-					properties: {
-						kind: 'action_executed',
-						command: data.command,
-						argument: data.argument,
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-			case 'editorClosed':
-				if ((<string>data.typeId) !== 'workbench.editors.gettingStartedInput') {
-					return undefined;
-				}
-				return {
-					event: 'vscode_getting_started',
-					properties: {
-						kind: 'editor_closed',
-						sessionID: data.sessionID,
-						timestamp: data.timestamp
-					},
-				};
-		}
-	} else if (source === 'window') {
-		switch (eventName) {
-			case 'remoteConnectionSuccess':
-			case 'remoteConnectionFailure':
-				type ConnectionProperties = {
-					state: string;
+			readAccessTracked = true;
+			return {
+				event: 'vscode_file_access',
+				properties: {
+					kind: 'read',
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'filePUT':
+			if (writeAccessTracked) {
+				return undefined;
+			}
+			writeAccessTracked = true;
+			return {
+				event: 'vscode_file_access',
+				properties: {
+					kind: 'write',
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'notification:show':
+			return {
+				event: 'vscode_notification',
+				properties: {
+					action: 'show',
+					notificationId: data.id,
+					source: data.source,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'notification:close':
+			return {
+				event: 'vscode_notification',
+				properties: {
+					action: 'close',
+					notificationId: data.id,
+					source: data.source,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'notification:hide':
+			return {
+				event: 'vscode_notification',
+				properties: {
+					action: 'hide',
+					notificationId: data.id,
+					source: data.source,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'notification:actionExecuted':
+			return {
+				event: 'vscode_notification',
+				properties: {
+					action: 'actionExecuted',
+					notificationId: data.id,
+					source: data.source,
+					actionLabel: data.actionLabel,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'settingsEditor.settingModified':
+			return {
+				event: 'vscode_update_configuration',
+				properties: {
+					key: data.key,
+					target: data.target,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'gettingStarted.ActionExecuted':
+			return {
+				event: 'vscode_getting_started',
+				properties: {
+					kind: 'action_executed',
+					command: data.command,
+					argument: data.argument,
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'editorClosed':
+			if ((<string>data.typeId) !== 'workbench.editors.gettingStartedInput') {
+				return undefined;
+			}
+			return {
+				event: 'vscode_getting_started',
+				properties: {
+					kind: 'editor_closed',
+					sessionID: data.sessionID,
+					timestamp: data.timestamp
+				},
+			};
+		case 'remoteConnectionSuccess':
+		case 'remoteConnectionFailure':
+			type ConnectionProperties = {
+				state: string;
+				// Time, in ms, until connected / connection failure
+				connectionTimeMs: number;
+				// Detailed error message of failure
+				error?: string;
+			};
+			return {
+				event: 'vscode_browser_remote_connection',
+				properties: {
+					state: getEventName(eventName),
 					// Time, in ms, until connected / connection failure
-					connectionTimeMs: number;
+					connectionTimeMs: data.connectionTimeMs,
 					// Detailed error message of failure
-					error?: string;
-				};
-				return {
-					event: 'vscode_browser_remote_connection',
-					properties: {
-						state: getEventName(eventName),
-						// Time, in ms, until connected / connection failure
-						connectionTimeMs: data.connectionTimeMs,
-						// Detailed error message of failure
-						error: data.message,
-					} as ConnectionProperties
-				};
-			case 'remoteConnectionLatency':
-				type ConnectionLatencyProperties = {
-					// Latency to the remote, in milliseconds
-					latencyMs?: number;
-				};
-				return {
-					event: 'vscode_browser_remote_connection_latency',
-					properties: {
-						latencyMs: data.latencyMs
-					} as ConnectionLatencyProperties
-				};
-			case 'remoteConnectionGain':
-			case 'remoteConnectionLost':
-			case 'remoteReconnectionWait':
-			case 'remoteReconnectionReload':
-			case 'remoteReconnectionRunning':
-			case 'remoteReconnectionPermanentFailure':
-				type ReconnectionProperties = {
-					event: string;
-					reconnectionToken: string;
-					millisSinceLastIncomingData?: number;
-					attempt?: number;
-					handled?: boolean;
-				};
-				return {
-					event: 'vscode_browser_remote_reconnection',
-					properties: {
-						event: getEventName(eventName),
-						reconnectionToken: data.reconnectionToken,
-						millisSinceLastIncomingData: data.millisSinceLastIncomingData,
-						attempt: data.attempt,
-						handled: data.handled,
-					} as ReconnectionProperties
-				};
-		}
+					error: data.message,
+				} as ConnectionProperties
+			};
+		case 'remoteConnectionLatency':
+			type ConnectionLatencyProperties = {
+				// Latency to the remote, in milliseconds
+				latencyMs?: number;
+			};
+			return {
+				event: 'vscode_browser_remote_connection_latency',
+				properties: {
+					latencyMs: data.latencyMs
+				} as ConnectionLatencyProperties
+			};
+		case 'remoteConnectionGain':
+		case 'remoteConnectionLost':
+		case 'remoteReconnectionWait':
+		case 'remoteReconnectionReload':
+		case 'remoteReconnectionRunning':
+		case 'remoteReconnectionPermanentFailure':
+			type ReconnectionProperties = {
+				event: string;
+				reconnectionToken: string;
+				millisSinceLastIncomingData?: number;
+				attempt?: number;
+				handled?: boolean;
+			};
+			return {
+				event: 'vscode_browser_remote_reconnection',
+				properties: {
+					event: getEventName(eventName),
+					reconnectionToken: data.reconnectionToken,
+					millisSinceLastIncomingData: data.millisSinceLastIncomingData,
+					attempt: data.attempt,
+					handled: data.handled,
+				} as ReconnectionProperties
+			};
 	}
 	return undefined;
 }
